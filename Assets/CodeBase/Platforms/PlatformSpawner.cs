81579aaee7a85aba9a6fc5bf79637004697b2686
x1mine <emptybrawl@gmail.com>
2023-07-04 08:06:53 +0300

    refactor PlatformSpawner

--- Assets/CodeBase/Platforms/PlatformSpawner.cs
+++ Assets/CodeBase/Platforms/PlatformSpawner.cs
@@ -1,52 +1,51 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using CodeBase.StaticData;
 using UnityEngine;
 
 namespace CodeBase.Platforms
 {
     public class PlatformSpawner : MonoBehaviour
     {
-        [SerializeField] private Platform _platform;
-        [SerializeField] private int _poolSize;
-
-        [Header("Random Position Settings")] [SerializeField]
-        private float _yMin;
-
-        [SerializeField] private float _yMax;
-        [SerializeField] private float _xMultiplier;
+        [SerializeField] private PlatformSpawnerConfig _config;
 
         private readonly Queue<Platform> _pool = new();
-        private bool _initialized;
         private Transform _observableTarget;
+        private UnityEngine.Camera _camera;
 
         private void Update()
         {
-            if (!_initialized)
+            var firstPlatform = _pool.Peek();
+            if (IsCloseToPlatform(firstPlatform))
                 return;
 
-            var firstPlatform = _pool.Peek();
-            if (_observableTarget.position.y - firstPlatform.transform.position.y < 10) return;
-
-            _pool.Dequeue();
-            firstPlatform.transform.position = CalculateSpawnPosition();
-            _pool.Enqueue(firstPlatform);
+            ReplacePlatform(firstPlatform);
         }
 
         public void Construct(Transform observableTarget)
         {
             _observableTarget = observableTarget;
-
-            for (var i = 0; i < _poolSize; i++) SpawnPlatform();
-
-            _initialized = true;
+            _camera = UnityEngine.Camera.main;
+            for (var i = 0; i < _config.PoolSize; i++)
+                SpawnPlatform();
         }
+
+        private bool IsCloseToPlatform(Platform firstPlatform) =>
+            _observableTarget.position.y - firstPlatform.transform.position.y < _config.MaxDistanceFromTarget;
 
         private void SpawnPlatform()
         {
             var spawnPosition = CalculateSpawnPosition();
 
-            var platform = Instantiate(_platform, spawnPosition, Quaternion.identity, transform);
+            var platform = Instantiate(_config.Platform, spawnPosition, Quaternion.identity, transform);
             _pool.Enqueue(platform);
+        }
+
+        private void ReplacePlatform(Platform firstPlatform)
+        {
+            _pool.Dequeue();
+            firstPlatform.transform.position = CalculateSpawnPosition();
+            _pool.Enqueue(firstPlatform);
         }
 
         private Vector2 CalculateSpawnPosition()
@@ -55,11 +54,13 @@ namespace CodeBase.Platforms
             if (_pool.Count == 0) return spawnPosition;
 
             var lastPlatform = _pool.Last();
-            spawnPosition.y = lastPlatform.transform.position.y + Random.Range(_yMin, _yMax);
+            spawnPosition.y = lastPlatform.transform.position.y + Random.Range(_config.RandomYMin, _config.RandomYMax);
             spawnPosition.x =
-                UnityEngine.Camera.main!
+                _camera
                     .ScreenToViewportPoint(
-                        new Vector3(Random.Range(-Screen.width * _xMultiplier, Screen.width * _xMultiplier), 0))
+                        new Vector3(
+                            Random.Range(-Screen.width * _config.RandomXMultiplier,
+                                Screen.width * _config.RandomXMultiplier), 0))
                     .x;
 
             return spawnPosition;
---
+++ Assets/CodeBase/StaticData/PlatformSpawnerConfig.cs
@@ -0,0 +1,23 @@
+﻿using CodeBase.Platforms;
+using UnityEngine;
+
+namespace CodeBase.StaticData
+{
+    [CreateAssetMenu(menuName = "World/Platform Spawner Config", fileName = "Platform Spawner Config")]
+    public class PlatformSpawnerConfig : ScriptableObject
+    {
+        [SerializeField] private Platform _platform;
+        [SerializeField] private int _poolSize;
+        [SerializeField] private float _maxDistanceFromTarget;
+        [SerializeField] private float _randomYMin;
+        [SerializeField] private float _randomYMax;
+        [SerializeField] private float _randomXMultiplier;
+
+        public Platform Platform => _platform;
+        public int PoolSize => _poolSize;
+        public float MaxDistanceFromTarget => _maxDistanceFromTarget;
+        public float RandomYMin => _randomYMin;
+        public float RandomYMax => _randomYMax;
+        public float RandomXMultiplier => _randomXMultiplier;
+    }
+}
